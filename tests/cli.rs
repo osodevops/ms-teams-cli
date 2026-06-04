913176fee2539de513ea154e6808ebd8d51581d8
Sion Smith <sion.smith@gmail.com>
2026-06-04 12:56:43 +0100

    Bump version to 0.2.4

--- tests/cli.rs
+++ tests/cli.rs
@@ -39,7 +39,7 @@ fn version_flag_works() {
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("0.2.3"));
+        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
 }
 
 #[test]
